403598919a141e09db44c5ad98c0c0e9f1e59d83
louisliao20000822 <louisliao20000822@gmail.com>
2022-09-22 13:05:52 +0800

    week2

--- Week-2/Assignment/assignment2.js
+++ Week-2/Assignment/assignment2.js
@@ -1,11 +1,13 @@
-const title = document.querySelector("#title-box");
-const btn = document.querySelector(".btn-click");
-const box = document.querySelector("#btn-box");
 
-title.addEventListener('click' , () => {
-    title.textContent = "Have a Good Time!"
-})
 
-btn.addEventListener('click' , () => {
-    box.style.display = "flex";
-})
+function delayedResult(n1, n2, delayTime, callback) {
+// your code here
+  setTimeout(callback,delayTime,n1+n2);
+
+}
+delayedResult(4, 5, 3000, function (result) {
+console.log(result);
+}); // 9 (4+5) will be shown in the console after 3 seconds
+delayedResult(-5, 10, 2000, function (result) {
+console.log(result);
+}); // 5 (-5+10) will be shown in the console after 2 seconds
