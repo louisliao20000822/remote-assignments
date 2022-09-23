e47c79da1ebf880fbdd3f1f852d5dfe10acc2fc0
louisliao20000822 <louisliao20000822@gmail.com>
2022-09-23 13:28:04 +0800

    week2

--- Week-2/Assignment/public/sum.js
+++ Week-2/Assignment/public/sum.js
@@ -1,5 +1,7 @@
 var xhr = new XMLHttpRequest();
 var value;
+const btn = document.querySelector("#Button");
+
 xhr.onreadystatechange  = function(){
     if(xhr.readyState === 4){
         if(xhr.status!= 200){
@@ -9,9 +11,9 @@ xhr.onreadystatechange  = function(){
         document.getElementById('ajax').innerHTML = xhr.responseText;
     }
 };
-function mysend(){
+btn.addEventListener('click' , () => {
     value = document.querySelector("#mytext").value;
     xhr.open("GET",`http://localhost:3000/data?number=${value}`);
     document.querySelector("#mytext").value = '';
     xhr.send();
-}
+});
